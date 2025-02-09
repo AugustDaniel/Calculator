ca61befb096c360893b8e91d237925bdda7816bd
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-02-09 14:01:56 +0100

    added buttons in html

---
+++ js/calculator.js
@@ -0,0 +1,25 @@
+export function add(num1, num2) {
+    return num1 + num2;
+}
+
+export function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+export function divide(num1, num2) {
+    return num1 / num2;
+}
+
+export function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+export function squareRoot(num) {
+    return Math.sqrt(num);
+}
+
+export function percentageOf(total, percentage) {
+    return (percentage / 100) * total;
+}
+
+
---
+++ js/script.js
@@ -0,0 +1,2 @@
+import * as calc from './calculator';
+
