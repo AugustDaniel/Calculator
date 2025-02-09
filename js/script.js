fa9c300ddf0df05944a47ac891d973aba8dbb1a3
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-02-09 16:47:17 +0100

    added values to buttons

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
-import * as calc from './calculator';
+import * as calc from './calculator.js';
 
+const currentDisplay = document.getElementById("current-display");
+const previousDisplay = document.getElementById("previous-display");
+const numbers = document.querySelectorAll("#buttons .number-button");
+const operators = document.querySelectorAll("#buttons .operator-button");
+
+console.log(calc.add(1,2))
