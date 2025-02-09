0bcb998a4507667687d4b52ed29f989e34c2b8a9
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2025-02-09 17:01:33 +0100

    added calculation object

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 import * as calc from './calculator.js';
 
+const calculation = {
+    previousNum1: 1,
+    previousOp: "+",
+    previousNum2: 1,
+    result: 2,
+    currentNum1: "",
+    currentOp: "",
+    currentNum2: "",
+}
+
 const currentDisplay = document.getElementById("current-display");
 const previousDisplay = document.getElementById("previous-display");
 const numbers = document.querySelectorAll("#buttons .number-button");
